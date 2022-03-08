3d3c235f522125dd054a04ead89ba19b48417bda
s.motamedi <s.motamedi@rayanbourse.ir>
2022-03-09 02:38:06 +0330

    Add Coupon Repository

---
+++ MicroservicesApp/src/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration configuration;
+        public DiscountRepository(IConfiguration _configuration)
+        {
+            configuration = _configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WHERE ProductName = @ProductName ", new { ProductName = productName });
+            if (coupon == null)
+                return new Coupon { ProductName = "No", Amount = 0, Description = "No" };
+            return coupon;
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

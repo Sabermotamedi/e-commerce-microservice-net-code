8585d2942710452b00c781a43c43dace781d0dfa
s.motamedi <s.motamedi@rayanbourse.ir>
2022-03-10 01:07:34 +0330

    Add DiscountController

--- MicroservicesApp/src/Discount/Discount.API/Controllers/CouponController.cs
+++ MicroservicesApp/src/Discount/Discount.API/Controllers/CouponController.cs
@@ -1,27 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Discount.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class CouponController : ControllerBase
-    {       
-       
-
-        public CouponController()
+    {
+        private readonly IDiscountRepository discountRepository;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CouponController"/> class.
+        /// </summary>
+        /// <param name="discountRepository">discountRepository</param>
+        public CouponController(IDiscountRepository discountRepository)
         {
-
+            this.discountRepository = discountRepository;
         }
 
-        [HttpGet]
-        public void Get()
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
         {
-           
+            var coupon = await this.discountRepository.GetDiscount(productName);
+            return this.Ok(coupon);
         }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await this.discountRepository.CreateDiscount(coupon);
+            return this.CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return this.Ok(await this.discountRepository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            return this.Ok(await this.discountRepository.DeleteDiscount(productName));
+        }
+
     }
 }

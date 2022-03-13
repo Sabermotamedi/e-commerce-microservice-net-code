dd9582ce3a392b7475824cc7781046197c9ab10b
s.motamedi <s.motamedi@rayanbourse.ir>
2022-03-14 01:39:15 +0330

    Task/#6 Add & clone Discount Project

---
+++ MicroservicesApp/src/Discount/Discount.Grpc/Discount.Grpc/Entities/Coupon.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Coupon.cs" company="SaberMotamedi">
+// Copyright (c) SaberMotamedi. All rights reserved.  Developed with 🖤
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Discount.Grpc.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}

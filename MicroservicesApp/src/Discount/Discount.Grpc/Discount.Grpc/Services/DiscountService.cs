9a434f0cb1db712d3969e5df7f761eb943e2ce6c
s.motamedi <s.motamedi@rayanbourse.ir>
2022-03-14 01:52:15 +0330

    Task/#6 Add Discount Service

---
+++ MicroservicesApp/src/Discount/Discount.Grpc/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository discountRepository;
+
+        public DiscountService(IDiscountRepository discount)
+        {
+            this.discountRepository = discount;
+        }
+        //public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        //{
+        //    var coupon = await discountRepository.GetDiscount(request.ProductName);
+        //    if (coupon == null)
+        //    {
+        //        throw new RpcException(new Status(StatusCode.NotFound, $"Discount with productName={request.ProductName} NotFound"));
+        //    }
+        //    var couponModel = _mapper.Map<CouponModel>(coupon);
+        //    return couponModel;
+        //}
+
+    }
+}
--- MicroservicesApp/src/Discount/Discount.Grpc/Discount.Grpc/Startup.cs
+++ MicroservicesApp/src/Discount/Discount.Grpc/Discount.Grpc/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,7 +34,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-               // endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<DiscountService>();
 
                 endpoints.MapGet("/", async context =>
                 {
